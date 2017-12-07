affc4697550e38ae818eac080466ef46477e7936
Nir S <nir.expobee@gmail.com>
2017-12-07 10:44:55 +0200

    add tasks to calendar

--- app/calendar/pianoDaysCtrl.js
+++ app/calendar/pianoDaysCtrl.js
@@ -23,24 +23,36 @@ var pianoDaysCtrl = pianoApp.controller("pianoDaysCtrl", function($rootScope,$sc
     $scope.notesList = []
 
     
-    $scope.LessonList = {
-        "day":"Sunday",
-        "hour":"09:00",
-        "student": "Nir"
-    }
+    $scope.LessonList = [
+                            {"day":"Sunday", "hour":"09:00", "student": "Nir"},
+
+                            {"day":"Monday", "hour":"12:00", "student": "Ranem"},
+                            
+                            {"day":"Thursday", "hour":"14:00", "student": "David"},
+
+                            {"day":"Sunday", "hour":"13:00", "student": "David"},
+                            
+                            {"day":"Monday", "hour":"17:00", "student": "Nir"},
+
+                            {"day":"Tuesday", "hour":"11:00", "student": "Ranem"},
+
+                            {"day":"Tuesday", "hour":"12:00","student": "Ranem"},
+                            
+                            {"day":"Thursday", "hour":"15:00", "student": "Karen"}
+                        ]
 
 
 
     $scope.getLesson = function (day,hour) {
     
-    
-            if ($scope.LessonList.day == this.day && $scope.LessonList.hour == this.hour) {
-                return $scope.LessonList.student + ", " + $scope.notesList[0].composer
+            for(var i=0; i<$scope.LessonList.length;i++){
+            if ($scope.LessonList[i].day == this.day && $scope.LessonList[i].hour == this.hour) {
+                return $scope.LessonList[i].student + ", " + $scope.notesList[0].composer
                 +", " + $scope.notesList[0].compositions[0];
         /*console.log("success: " + $scope.LessonList.student)*/
         }
         //console.log("fail: " + $scope.LessonList.day + " " + this.day + " " +$scope.LessonList.hour + " " + this.hour);
-    }
+    }}
 
 
 
